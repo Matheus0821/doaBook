73b1d875f67a8054d96c7fd21cc8913e3feac903
Kewin Souto Barros <kevinstbarros@gmail.com>
2021-02-22 16:12:23 -0300

    Criando model tema e configurando o repository

---
+++ doabook-api/src/main/java/com/hardtech/doabookapi/repository/TemasRepository.java
@@ -0,0 +1,10 @@
+
+package com.hardtech.doabookapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hardtech.doabookapi.models.Temas;
+
+public interface TemasRepository extends JpaRepository<Temas, Long> {
+
+}
